13a0197c688fd68a731afbdd45f66ec68c15d345
Наташа <Natasha@192.168.1.43>
2016-12-14 00:45:35 +0300

    Editing API

--- web/app/backend.php
+++ web/app/backend.php
@@ -56,9 +56,10 @@
 			$table=new \DB\SQL\Mapper(\F3::get('DB'),'sklad');						
 			$table->id_material=\F3::get("POST.id_material");
 			$table->id_ue=\F3::get("POST.id_ue");	
-			$table->value=\F3::get("POST.value");				
+			$table->quantity=\F3::get("POST.quantity");
+			$table->type=1;				
 			$table->save();	
-			$last_id = \F3::get('DB')->exec("SELECT id FROM agent ORDER BY id DESC LIMIT 1")[0]["id"];		
+			$last_id = \F3::get('DB')->exec("SELECT id FROM sklad ORDER BY id DESC LIMIT 1")[0]["id"];		
 			echo($last_id);
 			}
 			else {
@@ -80,8 +81,62 @@
 		}
 
 		function addZakaz($f3){			
-			$result = $f3->get("DB")->exec("");
-			echo($result);
-		}		
+			if(isset($_POST["id_trademark"])){
+				$table=new \DB\SQL\Mapper(\F3::get('DB'),'zakaz');									
+				$table->id_trademark=\F3::get("POST.id_trademark");	
+				$table->id_agent=\F3::get("POST.id_agent");	
+				$table->number=\F3::get("POST.number");					
+				$table->save();	
+				$last_id = \F3::get('DB')->exec("SELECT id FROM zakaz ORDER BY id DESC LIMIT 1")[0]["id"];
+				foreach($_POST["material"] as $material){
+					\F3::get('DB')->exec("INSERT INTO zakaz_material (id_zakaz, id_material, quantity) VALUES ($last_id, $material['id'], $material['quantity'])");
+				}
+				echo($last_id);
+			}
+			else {
+				echo("error");
+			}
+		}
+		
+		function removeFromSklad($f3){			
+			if (isset($_POST["id"])){
+			$table=new \DB\SQL\Mapper(\F3::get('DB'),'sklad');						
+			$table->id_material=\F3::get("POST.id_material");
+			$table->id_ue=\F3::get("POST.id_ue");	
+			$table->quantity=\F3::get("POST.quantity");
+			$table->type=0;					
+			$table->save();	
+			$last_id = \F3::get('DB')->exec("SELECT id FROM sklad ORDER BY id DESC LIMIT 1")[0]["id"];		
+			echo($last_id);
+			}
+			else {
+				echo("error");
+			}
+		}
+		//EDITING ITEMS
+		
+		function editZakaz($f3){			
+			if(isset($_POST["id_trademark"])){
+				$table=new \DB\SQL\Mapper(\F3::get('DB'),'zakaz');	
+				$table->load(array("id = ?", $_POST["id"]));								
+				$table->id_trademark=\F3::get("POST.id_trademark");	
+				$table->id_agent=\F3::get("POST.id_agent");	
+				$table->number=\F3::get("POST.number");					
+				$table->update();	
+				$last_id = \F3::get('DB')->exec("SELECT id FROM zakaz ORDER BY id DESC LIMIT 1")[0]["id"];
+				\F3::get('DB')->exec("DELETE FROM zakaz_material WHERE id_zakaz = ?", $_POST["id"]);
+				foreach($_POST["material"] as $material){
+					\F3::get('DB')->exec("INSERT INTO zakaz_material (id_zakaz, id_material, quantity) VALUES ($last_id, $material['id'], $material['quantity'])");
+				}
+				echo($last_id);
+			}
+			else {
+				echo("error");
+			}
+		}
+		
+		//REMOVING ITEMS
+		
+				
 
 	}
