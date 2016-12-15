67575184a67a408ab496fca1fe04142cd5084b35
pestovanatasha <nspestova@edu.hse.ru>
2016-12-15 13:01:03 +0300

    Adding classes and properties

--- ERPForYou.Data/Trademark.cs
+++ ERPForYou.Data/Trademark.cs
@@ -6,7 +6,9 @@ using System.Threading.Tasks;
 
 namespace ERPForYou.Data
 {
-    public class Class1
+    public class Trademark
     {
+        public int Id { get; set; }
+        public string Name { get; set; }
     }
 }
---
+++ ERPForYou.Data/Zakaz.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ERPForYou.Data
+{
+    class Zakaz
+    {
+        public Trademark Trademark { get; set; }
+        public int MyProperty { get; set; }
+        public Agent Agent { get; set; }
+        public int Quantity { get; set; }
+        public DateTime Datetime { get; set; }
+
+    }
+}
