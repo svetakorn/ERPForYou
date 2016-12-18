a9c594b41f667249e77f5ca36aa1036342e4c146
Parvin Tagieva <parvishka@icloud.com>
2016-12-18 03:44:23 +0300

    Some models added

---
+++ ERPForYou/Model/Zakaz_material.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ERPForYou.Model
+{
+    public class Zakaz_material
+    {
+        public int Id { get; set; }
+        public int Id_material { get; set; }
+        public int Id_zakaz { get; set; }
+        public int Quantity { get; set; }
+    }
+}
